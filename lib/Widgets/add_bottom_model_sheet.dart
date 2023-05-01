4f5a3a3a10d8f146f5b05dd34729596334a6a5df
ahmadkhedr <elemamm51@gmail.com>
2023-05-02 00:35:25 +0300

    create custom test field

--- lib/Widgets/add_bottom_model_sheet.dart
+++ lib/Widgets/add_bottom_model_sheet.dart
@@ -1,10 +1,22 @@
 import 'package:flutter/material.dart';
 
+import 'custom_test_filed.dart';
+
 class AddBottomModelSheet extends StatelessWidget {
   const AddBottomModelSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return  Container();
+    return Padding(
+      padding: const EdgeInsets.symmetric(vertical: 16.0, horizontal: 16.0),
+      child: Column(
+        children: const [
+          SizedBox(
+            height: 32,
+          ),
+          CustomTextFiled(),
+        ],
+      ),
+    );
   }
 }
---
+++ lib/Widgets/custom_test_filed.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter/src/widgets/placeholder.dart';
+
+import '../constants.dart';
+
+class CustomTextFiled extends StatelessWidget {
+  const CustomTextFiled({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      cursorColor: kPrimaryColor,
+      decoration: InputDecoration(
+          hintText: "Title",
+          border: buildBorder(),
+          enabledBorder: buildBorder(),
+          focusedBorder: buildBorder(kPrimaryColor)),
+    );
+  }
+
+  OutlineInputBorder buildBorder([color]) {
+    return OutlineInputBorder(
+        borderRadius: BorderRadius.circular(8.0),
+        borderSide: BorderSide(
+          color: color ?? Colors.white,
+        ));
+  }
+}
